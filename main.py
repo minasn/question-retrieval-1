a644d58d2d15c68085a7b02eff8a320e0006712b
Rhea Lin <rhealin@mit.edu>
2017-12-07 13:41:55 -0500

    Averaging hidden outputs

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import sys
 import argparse
 import corpus
 
+import numpy as np
+
+import torch
 import torch.nn as nn
 import torch.autograd as autograd
 
@@ -13,32 +16,76 @@ def main(args):
 
     training_batches = corpus.create_batches(ids_corpus, annotations, args.batch_size, padding_id)
     
-    lstm = nn.LSTM(input_size=200, hidden_size=200)
+    lstm = nn.LSTM(input_size=200, hidden_size=args.hidden_size)
+
+    # lstm tutorial: http://pytorch.org/tutorials/beginner/nlp/sequence_models_tutorial.html
+    # lstm documentation: http://pytorch.org/docs/master/nn.html?highlight=nn%20lstm#torch.nn.LSTM
+    # 
 
     for batch in training_batches:
         titles, bodies, triples = batch
+        title_length, title_num_questions = titles.shape
+        body_length, body_num_questions = bodies.shape
         title_embeddings, body_embeddings = corpus.get_embeddings(titles, bodies, vocab_map, embeddings)
         
         # title
-        title_inputs = [autograd.Variable(title_embeddings)]
-        title_inputs = torch.cat(title_inputs).view(len(title_inputs), 1, -1)
+        title_inputs = [autograd.Variable(torch.FloatTensor(title_embeddings))]
+        title_inputs = torch.cat(title_inputs).view(title_length, title_num_questions, -1)
 
-        title_hidden = (autograd.Variable(torch.zeros(1, 1, 3)),
-              autograd.Variable(torch.zeros((1, 1, 3))))
+        # checking title_inputs dimensions
+        # print len(title_inputs)
+        # print len(title_inputs[0])
+        # print len(title_inputs[0][0])
 
-        out, title_hidden = lstm(title_inputs, title_hidden)
+        title_hidden = (autograd.Variable(torch.zeros(1, title_num_questions, args.hidden_size)),
+              autograd.Variable(torch.zeros((1, title_num_questions, args.hidden_size))))
 
-        print out
-        print out.shape
+        title_out, title_hidden = lstm(title_inputs, title_hidden)
+
+        # print(len(title_out))
+        # print(len(title_out[0]))
+        # print(len(title_out[0][0]))
+
+        # average all words of each question from title_out
+        # title_out (max sequence length) x (batch size) x (hidden size)
+        average_title_out = average_questions(title_out, titles, padding_id)
+        # print "avg title out "
+        # print len(average_title_out)
+        # print "\n"
 
         # body
-        body_inputs = [autograd.Variable(body_embeddings)]
-        body_inputs = torch.cat(body_inputs).view(len(body_inputs), 1, -1)
+        body_inputs = [autograd.Variable(torch.FloatTensor(body_embeddings))]
+        body_inputs = torch.cat(body_inputs).view(body_length, body_num_questions, -1)
 
-        body_hidden = (autograd.Variable(torch.zeros(1, 1, 3)),
-              autograd.Variable(torch.zeros((1, 1, 3))))
+        body_hidden = (autograd.Variable(torch.zeros(1, body_num_questions, args.hidden_size)),
+              autograd.Variable(torch.zeros((1, body_num_questions, args.hidden_size))))
         
-        out, body_hidden = lstm(body_inputs, body_hidden)
+        body_out, body_hidden = lstm(body_inputs, body_hidden)
+
+        # print(len(body_out))
+        # print(len(body_out[0]))
+        # print(len(body_out[0][0]))
+
+        # average all words of each question from body_out
+        # body_out (max sequence length) x (batch size) x (hidden size)
+        average_body_out = average_questions(body_out, bodies, padding_id)
+        # print "avg body out "
+        # print len(average_body_out)
+        # print "\n"
+
+def average_questions(hidden, ids, padding_id, eps=1e-10):
+    """Average the outputs from the hidden states of questions, excluding padding.
+    """
+    # sequence (title or body) x questions x 1
+    mask = autograd.Variable(torch.from_numpy(1 * (ids != padding_id)).type(torch.FloatTensor).unsqueeze(2))
+
+    # questions x hidden (=200)
+    masked_sum = torch.sum(mask * hidden, dim=0)
+
+    # questions x 1
+    lengths = torch.sum(mask, dim=0)
+
+    return masked_sum / (lengths + eps)
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(sys.argv[0])
@@ -65,8 +112,11 @@ if __name__ == "__main__":
             type = int,
             default = 25
         )
+    argparser.add_argument("--hidden_size",
+            type = int,
+            default = 100
+        )
     
     args = argparser.parse_args()
     main(args)
-
     
